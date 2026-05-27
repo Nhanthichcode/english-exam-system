7908080bcda8fefdb6ff6b1e3caa36ba3663af52
Nhanthichcode <nhanlx151@gmail.com>
2026-05-27 23:09:26 +0700

    update examRoutes.js

--- server/src/router/examRoutes.js
+++ server/src/router/examRoutes.js
@@ -7,4 +7,6 @@ const { authMiddleware, checkRole} = require('../middlewares/authMiddleware');
 router.post('/create-templet', authMiddleware, checkRole([1, 2]), examController.initiateNewExam);
 router.get('/:id/take', authMiddleware, checkRole([3]), examController.getExamForStudent);
 router.post('/auto-save', authMiddleware, checkRole([3]), examController.autoSaveProgress);
+router.post('/submit', authMiddleware, checkRole([3]), examController.submitExamAndGrade);
+
 module.exports = router;

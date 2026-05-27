cef6279312033c8723f8ddb9b6091dafe8705fc4
Nhanthichcode <nhanlx151@gmail.com>
2026-05-27 22:16:16 +0700

    update authRoutes.js

--- server/src/router/authRoutes.js
+++ server/src/router/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/authController');
-
+const { authMiddleware, checkRole} = require('../middlewares/authMiddleware');
 // Đường dẫn: POST /api/auth/register
 router.post('/register', authController.register);
 
@@ -14,5 +14,18 @@ router.post('/logout', authController.logout);
 
 // Đường dẫn: POST /api/auth/refreshtoken (MỚI THÊM)
 router.post('/refreshtoken', authController.refreshToken);
+
+// 1. Trang cá nhân học viên: Bất kỳ ai đăng nhập thành công cũng vào được (role_id nào cũng được)
+router.get('/profile', authMiddleware, (req, res) => {
+    res.json({
+        message: "Chào mừng bạn đến với khu vực tài khoản cá nhân!",
+        user_data: req.user // Hiển thị dữ liệu đã được bóc tách từ Token
+    });
+});
+
+// 2. Khu vực quản trị: Chỉ cho phép tài khoản Admin (role_id = 1) được truy cập
+router.get('/admin-only', authMiddleware, checkRole([1]), (req, res) => {
+    res.json({ message: "Chào sếp Admin! Đây là dữ liệu tuyệt mật của hệ thống." });
+});
 
 module.exports = router;

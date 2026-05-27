e3838f6f6cc1c2dd7e10012cef9901063d0c15a4
Nhanthichcode <nhanlx151@gmail.com>
2026-05-27 23:54:20 +0700

    update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const examRoutes = require('./router/examRoutes');
 
 // Cấu hình Middlewares
 app.use(cors({
-    origin: 'http://localhost:3000', // URL mặc định của React sau này
+    origin: 'http://localhost:5173', // URL mặc định của React sau này
     credentials: true // Cho phép gửi cookie kèm theo request
 }));
 app.use(express.json()); // Cho phép Express đọc dữ liệu dạng JSON từ Client gửi lên

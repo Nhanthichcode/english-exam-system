4d61f4343c34b7a52649e150d4362993f08aa6fb
Nhanthichcode <nhanlx151@gmail.com>
2026-05-27 22:16:46 +0700

    feat: create authMiddleware.js

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+const db = require('../config/db');
+const jwt = require('jsonwebtoken');
+const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log('Token nhận được từ client:', token);
+if(!token){
+    return res.status(401).json({ message: 'Không tìm thấy quyền truy cập' });
+}
+try{
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    req.user = decoded;
+    next();
+}catch(error){
+    console.error('Lỗi xác thực token:', error);
+    return res.status(403).json({ message: 'Token hết hạn hoặc không hợp lệ' }); 
+}
+};
+const checkRole = (allowerRoles)=>{
+    return (req, res, next)=>{
+        if(!req.user || !allowerRoles.includes(req.user.role_id)){
+            return res.status(403).json({ message: 'Bạn không có quyền truy cập vào khu vực này' });
+        }
+        next();
+    };
+};
+
+module.exports = {
+    authMiddleware,
+    checkRole
+};

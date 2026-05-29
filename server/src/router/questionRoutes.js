f919c8b6db4baef384e5a2bdc368f786ea1b5335
Nhanthichcode <nhanlx151@gmail.com>
2026-05-29 23:49:28 +0700

    update questionRoutes.js

--- server/src/router/questionRoutes.js
+++ server/src/router/questionRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const questionController = require('../controllers/questionController');
+const { authMiddleware, checkRole } = require('../middlewares/authMiddleware');
 
-const { authMiddleware, checkRole} = require('../middlewares/authMiddleware');
 // create-group là tên custom, không phải tên mặc định của RESTful API, nó là phương thức 'questionController.createFullQuestionGroup'
 router.post('/create-group', authMiddleware, checkRole([1, 2]), questionController.createFullQuestionGroup);
-router.post('/create-single', authMiddleware, checkRole([1, 2]), questionController.createSingleQuestion);
+router.post('/create', authMiddleware, checkRole([1, 2]), questionController.createSingleQuestion);
 router.post('/import-excel', authMiddleware, checkRole([1, 2]), questionController.importQuestionsFromExcel);
-router.put('/:id', authMiddleware, checkRole([1, 2]), questionController.updateQuestion);
-router.delete('/:id', authMiddleware, checkRole([1, 2]), questionController.deleteSingleQuestion);
+router.get('/detail/:id', authMiddleware, checkRole([1, 2]), questionController.getQuestionDetail);
+router.put('/update/:id', authMiddleware, checkRole([1, 2]), questionController.updateQuestion);
+router.delete('/delete/:id', authMiddleware, checkRole([1, 2]), questionController.deleteSingleQuestion);
 router.delete('/bulk/delete', authMiddleware, checkRole([1, 2]), questionController.deleteMultipleQuestions);
+router.get('/admin-list', authMiddleware, checkRole([1, 2]), questionController.getAdminQuestions);
+
 
 module.exports = router;
